5d4c74b27d07b2724efb07536b0d81d4b9a75aa8
imSarthakGautam <sarthakgtm175@gmail.com>
2023-07-21 19:56:09 +0545

    Sending PostSection and  UserData from BE to FE

--- lec-jobs-be/src/app.js
+++ lec-jobs-be/src/app.js
@@ -6,7 +6,7 @@ const app= express();
 app.use(cors());
 const PORT = 5000;
 
-// http://localhost:500 then this function will be called
+// http://localhost:5000 then this function will be called
 app.get("/", (req,res)=>{
     res.status(200).send("This is response from DB");
 });
@@ -16,6 +16,11 @@ app.get("/api/v1/posts", (req,res)=>{
     const posts = fs.readFileSync("./data/posts.json", "utf-8").toString();
     res.status(200).send(posts);
 });
+
+app.get("/api/v1/user", (req, res) => {
+    const user = fs.readFileSync("./data/user.json", "utf-8").toString();
+    res.status(200).send(user);
+  });
 
 app.listen(PORT, ()=>{
     console.log("App is running on port" + PORT);
